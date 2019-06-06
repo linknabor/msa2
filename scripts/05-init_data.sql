6386ffa7a33acfdbc61b03789335ed4bcfb7f0f8
davidhardson <davidhardson@hotmail.com>
2019-06-06 14:59:01 +0800

    merge

---
+++ scripts/05-init_data.sql
@@ -0,0 +1,41 @@
+#地区信息表
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000001','北京市','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000002','天津市','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000003','山东省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000004','辽宁省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000005','山西省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000006','河南省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000007','河北省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000008','吉林省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000009','黑龙江省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000010','陕西省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000011','甘肃省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000012','青海省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000013','宁夏省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000014','新疆省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000015','海南省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000016','香港','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000017','澳门区','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000018','上海','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000019','重庆市','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000020','广东省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000021','广西省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000022','湖南省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000023','江苏省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000024','浙江省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000025','内蒙古','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000026','安徽省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000027','福建省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000028','江西省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000029','四川省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000030','贵州省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000031','云南省','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000032','西藏','1','100000000000');
+insert into msa_base_regin_info(ID,regin_name,regin_type,super_id) values ('100000000033','台湾','1','100000000000');
+
+
+
+
+
+
+
